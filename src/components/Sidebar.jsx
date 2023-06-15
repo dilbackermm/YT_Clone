5de5d834031b94892b522ff72df56ecb9941998d
Dilbacker <mmdilbacker@gmail.com>
2023-06-15 16:19:01 +0530

    updated sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,9 +20,10 @@ const Sidebar = () => (
     background: category.name === selectedCategory && '#FC1503' ,
     color: 'white'
    }}
+   key={category.name}
     >
-        <span>{category.icon}</span>
-        <span>{category.name}</span>
+        <span style={ {color: category.name === selectedCategory ? 'white' :' red', marginRight: '15px' } }>{category.icon}</span>
+        <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8'}}>{category.name}</span>
     </button>
 ))}
     </Stack>
