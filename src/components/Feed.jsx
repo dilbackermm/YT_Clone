e43e9c746dbd07df4bfbbf7c99f34a392eeb41c0
Dilbacker <mmdilbacker@gmail.com>
2023-06-15 15:51:02 +0530

    updating sidebar component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,22 @@
+import { Box, Stack, Typography } from '@mui/material'
 import React from 'react'
+import Sidebar from './Sidebar'
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+ <Stack
+ sx={{ flexDirection: { sx: "column", md:"row"} }}
+ >
+<Box sx={{ height: {sx: 'auto', md:'92vh' }, borderRight: '1px solid #3d3d3d', px: {sx:0, md: 2}}}>
+<Sidebar />
+</Box>
+<Typography
+className='copyright'
+variant='body2' sx={{ mt:1.5, color: '#fff'}}
+>
+Copyright 2022 JSM Media
+</Typography>
+ </Stack>
   )
 }
 
