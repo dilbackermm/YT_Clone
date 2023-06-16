64ec68b8e84fb5356d9fdb664948f8e27ad9b4e8
Dilbacker <mmdilbacker@gmail.com>
2023-06-16 18:08:25 +0530

    starting videoDetails section

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { Box, Stack } from '@mui/material'
+import React, { useEffect } from 'react'
+import ReactPlayer from 'react-player'
+import { useParams } from 'react-router-dom'
 
 const VideoDetail = () => {
+const { id } = useParams();
+
+useEffect(() => {
+
+},[id])
+
   return (
-    <div>VideoDetail</div>
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: 'column', md: 'row' }}>
+        <Box flex={1}>
+          <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
+            <ReactPlayer />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
